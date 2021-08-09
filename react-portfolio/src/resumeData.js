64062f078525a51f66f6330378892c1c0e06bfe2
shanemcf <shanemcfadden95@yahoo.com>
2021-08-09 10:19:19 -0500

    site working but CSS not implemented yet and some grammatical and user errors

---
+++ react-portfolio/src/resumeData.js
@@ -0,0 +1,93 @@
+let resumeData = {
+    "imagebaseurl":"https://shanemcf.github.io/",
+    "name": "Shane",
+    "role": "Full Satck Developer",
+    "linkedinId":"205996129",
+    "roleDescription": "I enjoy creating websites both front and back end, as well as troubleshooting and refactoring code once complete. In my free time I also enjoy learning about video game development",
+    "socialLinks":[
+    {
+    "name":"linkedin",
+    "url":"https://www.linkedin.com/in/shane-mcfadden-205996129/",
+    "className":"fa fa-linkedin"
+    },
+    {
+    "name":"github",
+    "url":"http://github.com/ivy555",
+    "className":"fa fa-github"
+    },
+    ],
+    "aboutme":"I am currently a Full Stack web developer honing my skills for both front and back end, and slowly learning other skills to help broaden my knowledge. ",
+    "address":"Texas",
+    "website":"https://shanemcf.github.io",
+    "education":[
+    {
+    "UniversityName":"University of Texas at Austin",
+    "specialization":"Full Stack Web Development",
+    "MonthOfPassing":"September",
+    "YearOfPassing":"2021",
+    "Achievements":"Creeation and implementation of over 30 projects ranging from front end, back end and full stack."
+    },
+    
+    ],
+    "work":[
+    {
+    "CompanyName":"Monster Vape",
+    "specialization":"Inventory Specialist",
+    "MonthOfLeaving":"N/A",
+    "YearOfLeaving":"N/A",
+    "Achievements":"Increasing revenue at storefront location while decreasing issues with inventory as well as im,plementing better structure to counts to help with Audit procedures."
+    },
+    {
+    "CompanyName":"Some Company",
+    "specialization":"Some specialization",
+    "MonthOfLeaving":"Jan",
+    "YearOfLeaving":"2018",
+    "Achievements":"Some Achievements"
+    }
+    ],
+    "skillsDescription":"My skills entail largely in Javascript but I have had some study with other languages.",
+    "skills":[
+    {
+    "skillname":"HTML5"
+    },
+    {
+    "skillname":"Javascript"
+    },
+    {
+    "skillname":"Reactjs"
+    }
+    ],
+    "portfolio":[
+    {
+    "name":"project1",
+    "description":"mobileapp",
+    "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+    "name":"project2",
+    "description":"mobileapp",
+    "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+    "name":"project3",
+    "description":"mobileapp",
+    "imgurl":"images/portfolio/project2.png"
+    },
+    {
+    "name":"project4",
+    "description":"mobileapp",
+    "imgurl":"images/portfolio/phone.jpg"
+    }
+    ],
+    // "testimonials":[
+    // {
+    // "description":"This is a sample testimonial",
+    // "name":"Some technical guy"
+    // },
+    // {
+    // "description":"This is a sample testimonial",
+    // "name":"Some technical guy"
+    // }
+    // ]
+    }
+    export default resumeData
